97677b6d9f7bb74fce7d9638f7bc49462e1f4a1c
Denis <elspy@mail.ru>
2022-09-24 22:55:25 +0300

    Создан блок ввода данных в программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Блок ввода исходных данных
+Console.Write("Введите количество элементов массива: ");
+int inputArrayCount = Convert.ToInt32(Console.ReadLine());
+string[] firstArray = new string[inputArrayCount];
+for(int i = 0; i < inputArrayCount; i++)
+{
+    if(inputArrayCount  > 0)
+    {
+        Console.Write($"Значение {i+1}-го элемента массива: ");
+        firstArray[i] = Console.ReadLine();
+    }
+}
+// Блок логики
+// Вывод результата
