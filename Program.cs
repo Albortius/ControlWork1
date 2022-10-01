d00819d9853cec50b2bd1c8081a6f2b2e4883618
Denis <elspy@mail.ru>
2022-10-01 04:26:22 +0300

    Внесены изменения в блок ввода данных в программу

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 Console.Write("Введите количество элементов массива: ");
 int inputArrayCount = Convert.ToInt32(Console.ReadLine());
 string[] firstArray = new string[inputArrayCount];
+string[] lastArray  = new string[inputArrayCount];
 for(int i = 0; i < inputArrayCount; i++)
 {
     if(inputArrayCount  > 0)
     {
         Console.Write($"Значение {i+1}-го элемента массива: ");
-        firstArray[i] = Console.ReadLine();
+        firstArray[i]   = Console.ReadLine();
+        lastArray[i]    = firstArray[i];
     }
 }
 // Блок логики
